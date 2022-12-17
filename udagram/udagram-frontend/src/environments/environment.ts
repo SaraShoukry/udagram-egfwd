16ec90b651b8922507c990783902950ceb13cc6a
SaraShoukry <s.yahia93@gmail.com>
2022-12-17 08:51:35 +0200

    update frontend deploy bash

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,9 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  // apiHost: 'http://localhost:8080/api/v0'
+  apiHost: "http://udagram-api-dev-1.eba-njhuwiyd.us-east-1.elasticbeanstalk.com/api/v0",
+
 };
 
 /*
